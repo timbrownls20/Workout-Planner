dff9c97bd3a2d027aad14db9a888001c5bf6a011
timbrownls20 <timbrownls26@hotmail.com>
2021-08-26 15:35:34 +1000

    workoutdata custom hook

--- src/components/WorkoutManager.js
+++ src/components/WorkoutManager.js
@@ -1,44 +1,45 @@
-import React, { useEffect, useReducer,useContext } from "react";
-import axios from "axios";
+import React, { useEffect, useContext } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faWindowClose } from "@fortawesome/free-regular-svg-icons";
-import workoutListReducer from "../reducers/workoutListReducer";
-import config from "../config/config";
-import { WorkoutListActions as Actions } from "../enums/actions";
 import Workout from "./Workout";
 import { FormStateContext } from "../config/FormStateContext";
 import { FormState } from "../enums/enums";
+import useWorkoutData from "../hooks/useWorkoutData";
 
 const WorkoutManager = () => {
-  const [workoutList, dispatch] = useReducer(workoutListReducer, []);
-  const {formState, setFormState } = useContext(FormStateContext);
+  const { formState, setFormState } = useContext(FormStateContext);
+  const { workoutList, loadWorkouts } = useWorkoutData();
 
   useEffect(() => {
     loadWorkouts();
-  }, []);
-
-  async function loadWorkouts() {
-    let route = `${config.Server}/workouts`;
-    let res = await axios.get(route);
-    dispatch({ type: Actions.LOAD_WORKOUTS, value: res.data });
-  }
+  }, [loadWorkouts]);
 
   return (
     <>
-    <div className="container-fluid mt-4">
-      <div className="row">
-        <div className="col-12 d-flex justify-content-center">
-          {workoutList.map(element => {
-            return <Workout workout={element} key={element.id} ></Workout>
-          })}  
+      <div className="container-fluid mt-4">
+        <div className="row">
+          <div className="col-12 d-flex justify-content-center">
+            {workoutList.map((element) => {
+              return <Workout workout={element} key={element.id}></Workout>;
+            })}
+          </div>
         </div>
       </div>
-    </div>
-    <div className={"overlay container-fluid d-flex justify-content-center align-items-center" + (formState === FormState.EDIT ? " overlay-show" : "")}>
-      <div className="d-flex justify-content-end">
-        <FontAwesomeIcon icon={faWindowClose} size="2x" className="close" onClick={() => setFormState(FormState.UNDEFINED)} />
+      <div
+        className={
+          "overlay container-fluid d-flex justify-content-center align-items-center" +
+          (formState === FormState.EDIT ? " overlay-show" : "")
+        }
+      >
+        <div className="d-flex justify-content-end">
+          <FontAwesomeIcon
+            icon={faWindowClose}
+            size="2x"
+            className="close"
+            onClick={() => setFormState(FormState.UNDEFINED)}
+          />
+        </div>
       </div>
-    </div>
     </>
   );
 };
---
+++ src/hooks/useWorkoutData.js
@@ -0,0 +1,19 @@
+import { useReducer } from "react";
+import axios from "axios";
+import workoutListReducer from "../reducers/workoutListReducer";
+import config from "../config/config";
+import { WorkoutListActions as Actions } from "../enums/actions";
+
+const useWorkoutData = () => {
+  const [workoutList, dispatch] = useReducer(workoutListReducer, []);
+
+  async function loadWorkouts() {
+    let route = `${config.Server}/workouts`;
+    let res = await axios.get(route);
+    dispatch({ type: Actions.LOAD_WORKOUTS, value: res.data });
+  }
+
+  return { workoutList, loadWorkouts };
+};
+
+export default useWorkoutData;
